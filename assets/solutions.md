698ce1fd08deaaf144c3dc09008068db65191d7f
ectospectral <132393822+ectospectral@users.noreply.github.com>
2025-09-29 11:40:49 -0700

    let's go

---
+++ assets/solutions.md
@@ -0,0 +1,3 @@
+Warden told me their account password is just password? Remind me to change that. Delete this comment when you're finished.
+
+Enlighten
---
+++ js/login.js
@@ -0,0 +1,35 @@
+
+//this is all temporary, im just testing stuff
+let passwordForm = document.querySelector('#frontpage-form2');
+
+let correctUser = 'warden';
+let correctPassword = 'password';
+
+let depthsPassword = 'enlighten';
+
+function enterPassword(){
+
+    if(document.querySelector('#warden').value != correctPassword && document.querySelector('#warden').value != depthsPassword && document.querySelector('#user').value != correctUser){
+        alert('Unrecognized User');
+        return false;
+    }
+    else if(document.querySelector('#warden').value === correctPassword && document.querySelector('#user').value != correctUser){
+        alert('Unrecognized User');
+        return false;
+    }
+    else if(document.querySelector('#warden').value != correctPassword && document.querySelector('#warden').value != depthsPassword && document.querySelector('#user').value === correctUser){
+        alert('Incorrect Password');
+        return false;
+    }
+    else if(document.querySelector('#warden').value === correctPassword && document.querySelector('#user').value === correctUser){
+        alert('Welcome back.');
+        passwordForm.action = 'account.html';
+        passwordForm.submit();
+    }
+    else if(document.querySelector('#warden').value === depthsPassword && document.querySelector('#user').value === correctUser){
+        alert('further still');
+        passwordForm.action = 'depths.html';
+        passwordForm.submit(); 
+    }
+    
+}
